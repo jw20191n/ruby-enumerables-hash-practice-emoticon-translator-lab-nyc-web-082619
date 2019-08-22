b8d9e02de1beb049c115d543470a1a16a5635729
Jiayi Wang <clairejw1015@gmail.com>
2019-08-22 22:40:08 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,11 +1,32 @@
-# require modules here
+require "yaml"
+  
+def load_library(path)
 
-def load_library
-  # code goes here
+  emoticons = YAML.load_file(path)
+  # NOTE
+  # => name of the emotions (angel, angry, etc.) are keys of the hash emoticons; 
+  # emoticons[key][0] = english emotions; emoticons[key][1] = japanese emotions
+  # {
+  #   "angle" => ["O:)", "☜(⌒▽⌒)☞"],
+  #   "angry" => [ , ].....
+  # }
+  
+  hash = { "get_meaning": { }, "get_emoticon": { } }
+  
+  # :get_meaning => {"☜(⌒▽⌒)☞" => ""angel",
+  # "japanese emoticons" => "key",...}
+  # :get_emoticon => {"emoticons[key][0]" (english) => "emoticons[key][1]"(japanese)}
+  
+  emoticons.each_key do | key |
+      hash[:get_meaning][emoticons[key][1]] = key
+      hash[:get_emoticon][emoticons[key][0]] = emoticons[key][1]
+      
+  end
+  hash
 end
 
-def get_japanese_emoticon
-  # code goes here
+def get_japanese_emoticon(path, emoticons)
+  dictionary = load_library(path)
 end
 
 def get_english_meaning
